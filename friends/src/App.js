f408c0cf38e1f149d800524a8aac131b357c0018
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-11-09 15:23:37 -0800

    Created and added a navbar to App

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,15 @@
+// Library Imports
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+
+// Component Imports
+import NavBar from "./components/NavBar";
 import Login from "./components/Login";
 
 const App = () => {
   return (
     <div className="App">
+      <NavBar />
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/" component={Login} />
