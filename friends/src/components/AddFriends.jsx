13d95e816f20f3dbe158f4038c1ecf1d8a3305ad
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-11-09 19:36:24 -0800

    Add friend form now adds a friend

--- friends/src/components/AddFriends.jsx
+++ friends/src/components/AddFriends.jsx
@@ -1,12 +1,71 @@
 // Library Imports
-import React from "react";
+import React, { useState, useContext } from "react";
+import FriendsContext from "../contexts/FriendsContext";
+import axiosWithAuth from "../utilities/axiosWithAuth";
 
 const AddFriends = () => {
+  const { friends, setFriends } = useContext(FriendsContext);
+  const [newFriend, setNewFriend] = useState({
+    id: friends.length + 1,
+    name: "",
+    age: null,
+    email: "",
+  });
+
+  const handleChange = (event) => {
+    setNewFriend({
+      ...newFriend,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axiosWithAuth()
+      .post("http://localhost:5000/api/friends", newFriend)
+      .then((response) => {
+        console.log("Friend has been submitted!");
+        setFriends(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      <h2>Add a friend!</h2>
       <label>
-        <input />
+        Name:
+        <input
+          type="text"
+          name="name"
+          placeholder="Friend's Name..."
+          value={newFriend.name}
+          onChange={handleChange}
+        />
       </label>
+      <label>
+        Age:
+        <input
+          type="text"
+          name="age"
+          placeholder="Friend's Age..."
+          value={newFriend.age}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          placeholder="Friend's Email..."
+          value={newFriend.email}
+          onChange={handleChange}
+        />
+      </label>
+      <button>Submit Friend</button>
     </form>
   );
 };
--- friends/src/components/FriendsList.jsx
+++ friends/src/components/FriendsList.jsx
@@ -2,23 +2,39 @@
 import React, { useEffect, useContext } from "react";
 import FriendsContext from "../contexts/FriendsContext";
 
+// Component Imports
+import AddFriends from "./AddFriends";
+
 // Utilities
 import axiosWithAuth from "../utilities/axiosWithAuth";
 
-const FriendsList = (props) => {
+const FriendsList = () => {
   const { friends, setFriends } = useContext(FriendsContext);
   useEffect(() => {
     axiosWithAuth()
       .get("http://localhost:5000/api/friends")
       .then((response) => {
-        console.log(response.data);
+        setFriends(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
 
-  return <div className="friends"></div>;
+  return (
+    <div className="friends">
+      {friends.map((friend, index) => {
+        return (
+          <div key={index}>
+            <p>{friend.name}</p>
+            <p>{friend.age}</p>
+            <p>{friend.email}</p>
+          </div>
+        );
+      })}
+      <AddFriends />
+    </div>
+  );
 };
 
 export default FriendsList;
